324580407c17e4574895527336f690c07d64c99b
PraveenRaoVP01-T <praveen.rao@tarento.com>
2025-12-08 18:02:10 +0530

    Update ApiEndpoints.java

--- gitlab-kpa-service/src/main/java/com/example/gitlab_kpa_service/utils/ApiEndpoints.java
+++ gitlab-kpa-service/src/main/java/com/example/gitlab_kpa_service/utils/ApiEndpoints.java
@@ -5,6 +5,7 @@ public class ApiEndpoints {
     public static final String GET_ALL_MERGE_REQUESTS_BY_PROJECT_ID = "/projects/%s/merge_requests";
     public static final String GET_ISSUES_BY_PROJECT_ID = "/projects/%s/issues";
     public static final String GET_RELATED_MR_BY_ISSUE_IID = "/projects/%s/issues/%s/related_merge_requests";
+    public static final String GET_COMMITS_BY_MERGE_REQUEST_IID = "/projects/%s/merge_requests/%s/commits";
     public static final String GET_ALL_COMMITS_BY_PROJECT_ID = "/projects/%s/repository/commits";
     public static final String GET_COMMIT_DATA_BY_ID = "/projects/%s/repository/commits/%s";
 }
